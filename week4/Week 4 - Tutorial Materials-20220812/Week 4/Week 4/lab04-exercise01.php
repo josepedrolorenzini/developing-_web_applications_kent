19e21ea33d58a062c5af74dbd09ba1f399fc1e04
jp lorenzini <joseplorenzini@gmail.com>
2022-08-15 08:35:33 +1000

    first commit

---
+++ week4/Week 4 - Tutorial Materials-20220812/Week 4/Week 4/lab04-exercise01.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Exercise 4-1</title>
+</head>
+<body>
+<h1>Regular HTML section (outside  tags)</h1>
+<p>You can type regular HTML here and it will show up</p>
+
+<h1>PHP section (inside  tags)</h1>
+This was output using PHP<br></body>
+</html>
---
+++ week4/Week 4 - Tutorial Materials-20220812/Week 4/Week 4/lab04-exercise02.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Exercise 4-2</title>
+</head>
+<body>
+<h1>Simple Calendar using Loops</h1>
+
+<table border=1>
+<tr>
+  <th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th>
+</tr>
+
+</table>
+
+
+</body>
+</html>
